208fa65109b1423bdc595be5ccddd88dcc772480
John Kodumal <john@catamorphic.com>
2014-09-05 15:37:53 -0700

    Bug fixes. Move unnecessary methods out of class

--- ldclient/ldclient.py
+++ ldclient/ldclient.py
@@ -6,6 +6,8 @@ from cachecontrol import CacheControl
 
 __version__ = "0.1"
 
+__LONG_SCALE__ = float(0xFFFFFFFFFFFFFFF)
+
 class Config(object):
 
   def __init__(self, baseUri):
@@ -16,7 +18,6 @@ class Config(object):
     return cls('https://app.launchdarkly.com')
 
 class LdClient(object):
-  __LONG_SCALE__ = float(0xFFFFFFFFFFFFFFF)
 
   def __init__(self, apiKey, config = Config.default()):
     self._apiKey = apiKey
@@ -34,61 +35,59 @@ class LdClient(object):
     else:
       return val
 
-  def param_for_user(dict, user): 
-    if ('key' in user):
-      idHash = user['key']
-    else:
-      return None
-    if ('secondary' in user):
-      idHash += "." + user['secondary']
-    hash = long(hashlib.sha1('%s.%s.%s', dict['key'], dict['salt'], idHash).hexdigest()[:15], 16)
-    return hash / __LONG_SCALE__
+def param_for_user(dict, user): 
+  if ('key' in user):
+    idHash = user['key']
+  else:
+    return None
+  if ('secondary' in user):
+    idHash += "." + user['secondary']
+  hash = long(hashlib.sha1('%s.%s.%s' % (dict['key'], dict['salt'], idHash)).hexdigest()[:15], 16)
+  result = hash / __LONG_SCALE__
+  return result;
 
-  def match_target(target, user):
-    attr = target['attribute']
-    if (attr == 'key' or attr == 'ip' or attr == 'country'):      
-      u_value = user[attr]
-      if (u_value is None):
-        return False
-      else:
-        return u_value in target['values']
-    else: # custom attribute
-      if (user['custom'] is None):
-        return False
-      u_value = user['custom'][attr]
-      if (u_value is None):
-        return False
-      if (isinstance(u_value, str) or isinstance(u_value, (float, int, long))):
-        return u_value in target['values']
-      elif (isinstance(u_value, collections.Sequence)):
-        return len(set(u_value).intersection(target['values'])) > 0
+def match_target(target, user):
+  attr = target['attribute']
+  if (attr == 'key' or attr == 'ip' or attr == 'country' and attr in user):      
+    u_value = user[attr]
+    return u_value in target['values']
+  else: # custom attribute
+    if ('custom' not in user):
       return False
-
-
-  def match_variation(variation, user):
-    for target in variation['targets']:
-      if match_target(target, user):
-        return True
+    if (attr not in user['custom']):
+      return False
+    u_value = user['custom'][attr]
+    if (isinstance(u_value, str) or isinstance(u_value, (float, int, long))):
+      return u_value in target['values']
+    elif (isinstance(u_value, collections.Sequence)):
+      return len(set(u_value).intersection(target['values'])) > 0
     return False
 
-  def evaluate(dict, user):
-    if (not dict['on']): 
-      return None
-    param = param_for_user(dict, user)
-    if (param is None):
-      return None
 
-    for variation in dict['variations']:
-      if (match_variation(variation, user)):
-        return variation['value']
+def match_variation(variation, user):
+  for target in variation['targets']:
+    if match_target(target, user):
+      return True
+  return False
 
-    sum = 0.0
-    for variation in dict['variations']:
-      sum += float(variation['weight']) / 100.0
-      if (param < sum):
-        return variation['value']
+def evaluate(dict, user):
+  if (not dict['on']): 
+    return None
+  param = param_for_user(dict, user)
+  if (param is None):
+    return None
 
-    return False
+  for variation in dict['variations']:
+    if (match_variation(variation, user)):
+      return variation['value']
+
+  sum = 0.0
+  for variation in dict['variations']:
+    sum += float(variation['weight']) / 100.0
+    if (param < sum):
+      return variation['value']
+
+  return False
 
 
 
