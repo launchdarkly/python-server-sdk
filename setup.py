aa66ad5215153b963e71f2680f1abc22d8bcf44b
John Kodumal <john@catamorphic.com>
2014-09-05 16:31:41 -0700

    Set up reqs in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt')
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
 setup(
     name='ldclient-py',
     version='0.2',
@@ -12,6 +21,7 @@ setup(
     url='https://github.com/launchdarkly/python-client',
     description='LaunchDarkly SDK for Python',
     long_description='LaunchDarkly SDK for Python',
+    install_requires=reqs,
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
