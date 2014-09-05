6b367001e90a35d244acbaf3ebdf0408f5cace15
John Kodumal <john@catamorphic.com>
2014-09-05 15:24:32 -0700

    More work on python client

--- ldclient/ldclient.py
+++ ldclient/ldclient.py
@@ -16,6 +16,7 @@ class Config(object):
     return cls('https://app.launchdarkly.com')
 
 class LdClient(object):
+  __LONG_SCALE__ = float(0xFFFFFFFFFFFFFFF)
 
   def __init__(self, apiKey, config = Config.default()):
     self._apiKey = apiKey
@@ -24,27 +25,69 @@ class LdClient(object):
 
   def get_flag(self, key, user, default = False):
     h = {'Authorization': 'api_key ' + self._apiKey,
-          'User-Agent', 'PythonClient/' + __version__}
+          'User-Agent': 'PythonClient/' + __version__}
     r = self._session.get(self._config._baseUri + '/api/eval/features/' + key, headers = h)
     dict = r.json()
-    if (not dict['on']): 
-      return false
     val = evaluate(dict, user)
-    if (val is None or user is None):
+    if (val is None):
       return default
     else:
       return val
 
   def param_for_user(dict, user): 
-    if (user['key'] is not None):
+    if ('key' in user):
       idHash = user['key']
     else:
       return None
-    if (user['secondary'] is not None):
+    if ('secondary' in user):
       idHash += "." + user['secondary']
-    hash = hashlib.sha1(dict['key'] + '.' + dict['salt'] + '.' + idHash)[0:15]
+    hash = long(hashlib.sha1('%s.%s.%s', dict['key'], dict['salt'], idHash).hexdigest()[:15], 16)
+    return hash / __LONG_SCALE__
+
+  def match_target(target, user):
+    attr = target['attribute']
+    if (attr == 'key' or attr == 'ip' or attr == 'country'):      
+      u_value = user[attr]
+      if (u_value is None):
+        return False
+      else:
+        return u_value in target['values']
+    else: # custom attribute
+      if (user['custom'] is None):
+        return False
+      u_value = user['custom'][attr]
+      if (u_value is None):
+        return False
+      if (isinstance(u_value, str) or isinstance(u_value, (float, int, long))):
+        return u_value in target['values']
+      elif (isinstance(u_value, collections.Sequence)):
+        return len(set(u_value).intersection(target['values'])) > 0
+      return False
+
+
+  def match_variation(variation, user):
+    for target in variation['targets']:
+      if match_target(target, user):
+        return True
+    return False
 
   def evaluate(dict, user):
+    if (not dict['on']): 
+      return None
+    param = param_for_user(dict, user)
+    if (param is None):
+      return None
+
+    for variation in dict['variations']:
+      if (match_variation(variation, user)):
+        return variation['value']
+
+    sum = 0.0
+    for variation in dict['variations']:
+      sum += float(variation['weight']) / 100.0
+      if (param < sum):
+        return variation['value']
+
     return False
 
 
