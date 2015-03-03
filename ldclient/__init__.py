06736abd19a776baef04d4842f72d6226d49b6de
Patrick Kaeding <pkaeding@launchdarkly.com>
2015-03-03 11:59:44 -0800

    added log statement when retrying

--- ldclient/__init__.py
+++ ldclient/__init__.py
@@ -36,7 +36,7 @@ class Consumer(object):
         pass
 
     def send(self, events):
-        def do_send(retrying):
+        def do_send(should_retry):
             try: 
                 if isinstance(events, dict):
                     body = [events]
@@ -47,13 +47,14 @@ class Consumer(object):
                 r = self._session.post(uri, headers = hdrs, timeout = (self._config._connect, self._config._read), data=json.dumps(body))
                 r.raise_for_status()
             except ProtocolError as e:
-                if not retrying:
-                    do_send(True)
+                if should_retry:
+                    logging.warning('ProtocolError exception caught while sending events. Retrying.')
+                    do_send(False)
                 else:
                     logging.exception('Unhandled exception in consumer. Analytics events were not processed.')
             except:
                 logging.exception('Unhandled exception in consumer. Analytics events were not processed.')
-        do_send(False)
+        do_send(True)
 
 class AbstractBufferedConsumer(object):
     def __init__(self, capacity, interval):
@@ -150,7 +151,7 @@ class LDClient(object):
         self._consumer.flush()
 
     def get_flag(self, key, user, default=False):
-        def do_get_flag(retrying):
+        def do_get_flag(should_retry):
             try:
                 if self._offline:
                     return default
@@ -158,15 +159,16 @@ class LDClient(object):
                 self._send({'kind': 'feature', 'key': key, 'user': user, 'value': val})
                 return val
             except ProtocolError as e:
-                if not retrying:
-                    do_send(True)
+                if should_retry:
+                    logging.warning('ProtocolError exception caught while getting flag. Retrying.')
+                    do_send(False)
                 else:
                     logging.exception('Unhandled exception in get_flag. Returning default value for flag.')
                     return default
             except:
                 logging.exception('Unhandled exception in get_flag. Returning default value for flag.')
                 return default
-        return do_get_flag(False)
+        return do_get_flag(True)
 
     def _get_flag(self, key, user, default):
         hdrs = _headers(self._api_key)
