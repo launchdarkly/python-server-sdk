e7330222029ec24137903c8474343e8add4d0b95
Patrick Kaeding <pkaeding@launchdarkly.com>
2015-03-05 17:11:56 -0800

    bumped version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class PyTest(Command):
 
 setup(
     name='ldclient-py',
-    version='0.15.0',
+    version='0.15.1',
     author='Catamorphic Co.',
     author_email='team@catamorphic.com',
     packages=['ldclient'],
