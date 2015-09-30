e56c91546b836a1eaf4c369b5b239c1e615bfd4a
Kevin Wu <kevin.wu@launchdarkly.com>
2015-09-30 14:21:31 -0700

    removed extraneous files

--- _build/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to ld_python_client&#8217;s documentation!"],titleterms:{document:0,indic:0,ld_python_cli:0,tabl:0,welcom:0}})
