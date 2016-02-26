816fc046007bd2302817317dac9f548d4b2c48a7
Patrick Kaeding <pkaeding@launchdarkly.com>
2016-02-26 15:56:35 -0800

    releasing 0.20.1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class PyTest(Command):
 
 setup(
     name='ldclient-py',
-    version='0.20.0',
+    version='0.20.1',
     author='Catamorphic Co.',
     author_email='team@catamorphic.com',
     packages=['ldclient'],
