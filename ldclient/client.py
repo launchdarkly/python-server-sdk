989878dca4ae24869fc12e4e19b813448b077107
Dan Richelson <dan@launchdarkly.com>
2016-08-03 10:48:37 -0700

    Change string encoding

--- ldclient/client.py
+++ ldclient/client.py
@@ -251,7 +251,7 @@ class LDClient(object):
     def secure_mode_hash(self, user):
         if user.get('key', "") == "":
             return ""
-        return hmac.new(bytes(self._api_key), bytes(user.get('key')), hashlib.sha256).hexdigest()
+        return hmac.new(self._api_key.encode(), user.get('key').encode(), hashlib.sha256).hexdigest()
 
     @staticmethod
     def _sanitize_user(user):
