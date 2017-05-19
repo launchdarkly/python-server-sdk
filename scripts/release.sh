95ccc40c16c93d841b475723722c380bf3a22970
Dan Richelson <dan@launchdarkly.com>
2017-05-19 09:55:07 -0700

    Add release to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-# This script updates the version for the ldclient library. It does not actually perform a release.
+# This script updates the version for the ldclient library and releases it to PyPi
+# It will only work if you have the proper credentials set up in ~/.pypirc
+
 # It takes exactly one argument: the new version.
 # It should be run from the root of this git repo like this:
 #   ./scripts/release.sh 4.0.9
 
+# When done you should commit and push the changes made.
+
 set -uxe
-echo "Starting python-client version update"
+echo "Starting python-client release."
 
 VERSION=$1
 
@@ -17,4 +21,6 @@ SETUP_PY_TEMP=./setup.py.tmp
 sed "s/ldclient_version=.*/ldclient_version='${VERSION}'/g" setup.py > ${SETUP_PY_TEMP}
 mv ${SETUP_PY_TEMP} setup.py
 
-echo "Done with python-client version update"
+python setup.py sdist upload
+
+echo "Done with python-client release"
