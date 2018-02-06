f03aaa1398cc7dbebd14a22d9038789f10de19d0
Eli Bishop <eli@launchdarkly.com>
2018-02-06 14:11:31 -0800

    fix bug in flag evals - putting wrong flag in "prereqOf"

--- ldclient/flag.py
+++ ldclient/flag.py
@@ -44,7 +44,7 @@ def _evaluate(flag, user, feature_store, prereq_events=None):
             failed_prereq = prereq
 
         event = {'kind': 'feature', 'key': prereq.get('key'), 'user': user,
-                 'value': prereq_value, 'version': prereq_flag.get('version'), 'prereqOf': prereq.get('key')}
+                 'value': prereq_value, 'version': prereq_flag.get('version'), 'prereqOf': flag.get('key')}
         events.append(event)
 
     if failed_prereq is not None:
