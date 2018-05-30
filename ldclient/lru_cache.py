8b0482f920d06c4f3a9c6b48cb94bb143c15b0d7
Eli Bishop <eli@launchdarkly.com>
2018-05-30 13:13:07 -0700

    fix backported OrderedDict implementation

--- ldclient/lru_cache.py
+++ ldclient/lru_cache.py
@@ -5,10 +5,32 @@ Values are only refreshed by writing, not by reading. Not thread-safe.
 
 from collections import OrderedDict
 
+
+# Backport of Python 3.2 move_to_end method which doesn't exist in 2.7
+class OrderedDictWithReordering(OrderedDict):
+    if not hasattr(OrderedDict, 'move_to_end'):
+        # backport of Python 3.2 logic
+        def move_to_end(self, key, last=True):
+            link_prev, link_next, key = link = self._OrderedDict__map[key]
+            link_prev[1] = link_next
+            link_next[0] = link_prev
+            root = self._OrderedDict__root
+            if last:
+                last = root[0]
+                link[0] = last
+                link[1] = root
+                last[1] = root[0] = link
+            else:
+                first = root[1]
+                link[0] = root
+                link[1] = first
+                root[1] = first[0] = link
+
+
 class SimpleLRUCache(object):
     def __init__(self, capacity):
         self.capacity = capacity
-        self.cache = OrderedDict()
+        self.cache = OrderedDictWithReordering()
 
     def get(self, key):
         return self.cache.get(key)
@@ -20,34 +42,10 @@ class SimpleLRUCache(object):
     def put(self, key, value):
         found = (key in self.cache)
         if found:
-            SimpleLRUCache._move_to_end(self.cache, key)
+            self.cache.move_to_end(key)
         else:
             if len(self.cache) >= self.capacity:
                 self.cache.popitem(last=False)
         self.cache[key] = value
         return found
 
-    if hasattr(OrderedDict, 'move_to_end'):
-        def _move_to_end(od, key, last=True):
-            od.move_to_end(key, last)
-    else:
-        # backport of Python 3.2 logic
-        def _move_to_end(od, key, last=True):
-            '''Move an existing element to the end (or beginning if last==False).
-            Raises KeyError if the element does not exist.
-            When last=True, acts like a fast version of self[key]=self.pop(key).
-            '''
-            link_prev, link_next, key = link = od.__map[key]
-            link_prev[1] = link_next
-            link_next[0] = link_prev
-            root = od.__root
-            if last:
-                last = root[0]
-                link[0] = last
-                link[1] = root
-                last[1] = root[0] = link
-            else:
-                first = root[1]
-                link[0] = root
-                link[1] = first
-                root[1] = first[0] = link
