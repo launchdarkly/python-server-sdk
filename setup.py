a495b8c3f18c6efa56314c186962834fc4923075
LaunchDarklyCI <dev@launchdarkly.com>
2018-09-25 00:13:08 +0000

    Preparing for release of version 6.4.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def parse_requirements(filename):
     return [line for line in lineiter if line and not line.startswith("#")]
 
 
-ldclient_version='6.4.1'
+ldclient_version='6.4.2'
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements('requirements.txt')
