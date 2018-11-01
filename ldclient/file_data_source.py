ae8b25eb33ad3dbca21231f22ece4a96694f731c
Eli Bishop <eli@launchdarkly.com>
2018-11-01 15:04:39 -0700

    implement file data source, not including auto-update

---
+++ ldclient/file_data_source.py
@@ -0,0 +1,95 @@
+import json
+import six
+import traceback
+
+have_yaml = False
+try:
+    import yaml
+    have_yaml = True
+except ImportError:
+    pass
+
+from ldclient.interfaces import UpdateProcessor
+from ldclient.util import log
+from ldclient.versioned_data_kind import FEATURES, SEGMENTS
+
+
+class FileDataSource(UpdateProcessor):
+    @classmethod
+    def factory(cls, **kwargs):
+        return lambda config, store, ready : FileDataSource(store, kwargs, ready)
+    
+    def __init__(self, store, options, ready):
+        self._store = store
+        self._ready = ready
+        self._inited = False
+        self._paths = options.get('paths', [])
+        if isinstance(self._paths, six.string_types):
+            self._paths = [ self._paths ]
+
+    def start(self):
+        self._load_all()
+
+        # We will signal readiness immediately regardless of whether the file load succeeded or failed -
+        # the difference can be detected by checking initialized()
+        self._ready.set() 
+
+    def stop(self):
+        pass
+
+    def initialized(self):
+        return self._inited
+
+    def _load_all(self):
+        all_data = { FEATURES: {}, SEGMENTS: {} }
+        print "Loading: %s" % self._paths
+        for path in self._paths:
+            try:
+                self._load_file(path, all_data)
+            except Exception as e:
+                log.error('Unable to load flag data from "%s": %s' % (path, repr(e)))
+                traceback.print_exc()
+                return
+        print "Initing: %s" % all_data
+        self._store.init(all_data)
+        self._inited = True
+    
+    def _load_file(self, path, all_data):
+        content = None
+        with open(path, 'r') as f:
+            content = f.read()
+        parsed = self._parse_content(content)
+        for key, flag in six.iteritems(parsed.get('flags', {})):
+            self._add_item(all_data, FEATURES, flag)
+        for key, value in six.iteritems(parsed.get('flagValues', {})):
+            self._add_item(all_data, FEATURES, self._make_flag_with_value(key, value))
+        for key, segment in six.iteritems(parsed.get('segments', {})):
+            self._add_item(all_data, SEGMENTS, segment)
+    
+    def _parse_content(self, content):
+        if have_yaml:
+            if content.strip().startswith("{"):
+                print("json: %s" % content)
+                return json.loads(content)
+            else:
+                return yaml.load(content)
+        print("json: %s" % content)
+        return json.loads(content)
+    
+    def _add_item(self, all_data, kind, item):
+        items = all_data[kind]
+        key = item.get('key')
+        if items.get(key) is None:
+            items[key] = item
+        else:
+            raise Exception('In %s, key "%s" was used more than once' % (kind.namespace, key))
+
+    def _make_flag_with_value(self, key, value):
+        return {
+            'key': key,
+            'on': True,
+            'fallthrough': {
+                'variation': 0
+            },
+            'variations': [ value ]
+        }
---
+++ testing/test_file_data_source.py
@@ -0,0 +1,205 @@
+import json
+import os
+import pytest
+import tempfile
+import threading
+import time
+
+from ldclient.client import LDClient
+from ldclient.config import Config
+from ldclient.feature_store import InMemoryFeatureStore
+from ldclient.file_data_source import FileDataSource
+from ldclient.versioned_data_kind import FEATURES, SEGMENTS
+
+
+all_flag_keys = [ 'flag1', 'flag2' ]
+all_properties_json = '''
+  {
+    "flags": {
+      "flag1": {
+        "key": "flag1",
+        "on": true,
+        "fallthrough": {
+          "variation": 2
+        },
+        "variations": [ "fall", "off", "on" ]
+      }
+    },
+    "flagValues": {
+      "flag2": "value2"
+    },
+    "segments": {
+      "seg1": {
+        "key": "seg1",
+        "include": ["user1"]
+      }
+    }
+  }
+'''
+
+all_properties_yaml = '''
+---
+flags:
+  flag1:
+    key: flag1
+    "on": true
+flagValues:
+  flag2: value2
+segments:
+  seg1:
+    key: seg1
+    include: ["user1"]
+'''
+
+flag_only_json = '''
+  {
+    "flags": {
+      "flag1": {
+        "key": "flag1",
+        "on": true,
+        "fallthrough": {
+          "variation": 2
+        },
+        "variations": [ "fall", "off", "on" ]
+      }
+    }
+  }
+'''
+
+segment_only_json = '''
+  {
+    "segments": {
+      "seg1": {
+        "key": "seg1",
+        "include": ["user1"]
+      }
+    }
+  }
+'''
+
+fds = None
+store = None
+ready = None
+
+
+def setup_function():
+    global fds, store, ready
+    store = InMemoryFeatureStore()
+    ready = threading.Event()
+
+def teardown_function():
+    if fds is not None:
+        fds.stop()
+
+def make_temp_file(content):
+    f, path = tempfile.mkstemp()
+    os.write(f, content)
+    os.close(f)
+    return path
+
+def replace_file(path, content):
+    with open(path, 'w') as f:
+        f.write(content)
+
+def test_does_not_load_data_prior_to_start():
+    path = make_temp_file('{"flagValues":{"key":"value"}}')
+    try:
+        fds = FileDataSource.factory(paths = path)(Config(), store, ready)
+        assert ready.is_set() is False
+        assert fds.initialized() is False
+        assert store.initialized is False
+    finally:
+        os.remove(path)
+
+def test_loads_flags_on_start_from_json():
+    path = make_temp_file(all_properties_json)
+    try:
+        fds = FileDataSource.factory(paths = path)(Config(), store, ready)
+        fds.start()
+        assert store.initialized is True
+        assert sorted(list(store.all(FEATURES, lambda x: x).keys())) == all_flag_keys
+    finally:
+        os.remove(path)
+
+def test_loads_flags_on_start_from_yaml():
+    path = make_temp_file(all_properties_yaml)
+    try:
+        fds = FileDataSource.factory(paths = path)(Config(), store, ready)
+        fds.start()
+        assert store.initialized is True
+        assert sorted(list(store.all(FEATURES, lambda x: x).keys())) == all_flag_keys
+    finally:
+        os.remove(path)
+
+def test_sets_ready_event_and_initialized_on_successful_load():
+    path = make_temp_file(all_properties_json)
+    try:
+        fds = FileDataSource.factory(paths = path)(Config(), store, ready)
+        fds.start()
+        assert fds.initialized() is True
+        assert ready.is_set() is True
+    finally:
+        os.remove(path)
+
+def test_sets_ready_event_and_does_not_set_initialized_on_unsuccessful_load():
+    bad_file_path = 'no-such-file'
+    fds = FileDataSource.factory(paths = bad_file_path)(Config(), store, ready)
+    fds.start()
+    assert fds.initialized() is False
+    assert ready.is_set() is True
+
+def test_can_load_multiple_files():
+    path1 = make_temp_file(flag_only_json)
+    path2 = make_temp_file(segment_only_json)
+    try:
+        fds = FileDataSource.factory(paths = [ path1, path2 ])(Config(), store, ready)
+        fds.start()
+        assert len(store.all(FEATURES, lambda x: x)) == 1
+        assert len(store.all(SEGMENTS, lambda x: x)) == 1
+    finally:
+        os.remove(path1)
+        os.remove(path2)
+
+def test_does_not_allow_duplicate_keys():
+    path1 = make_temp_file(flag_only_json)
+    path2 = make_temp_file(flag_only_json)
+    try:
+        fds = FileDataSource.factory(paths = [ path1, path2 ])(Config(), store, ready)
+        fds.start()
+        assert len(store.all(FEATURES, lambda x: x)) == 0
+    finally:
+        os.remove(path1)
+        os.remove(path2)
+
+def test_does_not_reload_modified_file_if_auto_update_is_off():
+    path = make_temp_file(flag_only_json)
+    try:
+        fds = FileDataSource.factory(paths = path)(Config(), store, ready)
+        fds.start()
+        assert len(store.all(SEGMENTS, lambda x: x)) == 0
+        time.sleep(0.5)
+        replace_file(path, segment_only_json)
+        time.sleep(0.5)
+        assert len(store.all(SEGMENTS, lambda x: x)) == 0
+    finally:
+        os.remove(path)
+
+def test_evaluates_full_flag_with_client_as_expected():
+    path = make_temp_file(all_properties_json)
+    try:
+        fds = FileDataSource.factory(paths = path)
+        client = LDClient(config=Config(update_processor_class = fds, send_events = False))
+        value = client.variation('flag1', { 'key': 'user' }, '')
+        assert value == 'on'
+    finally:
+        os.remove(path)
+
+def test_evaluates_simplified_flag_with_client_as_expected():
+    path = make_temp_file(all_properties_json)
+    try:
+        fds = FileDataSource.factory(paths = path)
+        client = LDClient(config=Config(update_processor_class = fds, send_events = False))
+        value = client.variation('flag2', { 'key': 'user' }, '')
+        assert value == 'value2'
+    finally:
+        os.remove(path)
