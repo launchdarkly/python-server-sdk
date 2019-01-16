78b611865e82278339e8fed4a3fe84ee24b24466
Eli Bishop <eli@launchdarkly.com>
2019-01-15 16:04:39 -0800

    minor doc fixes

--- ldclient/integrations.py
+++ ldclient/integrations.py
@@ -15,8 +15,21 @@ class DynamoDB(object):
                           caching=CacheConfig.default()):
         """Creates a DynamoDB-backed implementation of `:class:ldclient.feature_store.FeatureStore`.
 
+        To use this method, you must first install the `boto3` package containing the AWS SDK gems.
+        Then, put the object returned by this method into the `feature_store` property of your
+        client configuration (:class:ldclient.config.Config).
+
+        Note that the DynamoDB table must already exist; the LaunchDarkly SDK does not create the table
+        automatically, because it has no way of knowing what additional properties (such as permissions
+        and throughput) you would want it to have. The table must have a partition key called
+        "namespace" and a sort key called "key", both with a string type.
+
+        By default, the DynamoDB client will try to get your AWS credentials and region name from
+        environment variables and/or local configuration files, as described in the AWS SDK documentation.
+        You may also pass configuration settings in `dynamodb_opts`.
+
         :param string table_name: The name of an existing DynamoDB table
-        :param string prefix: An optional namespace prefix to be prepended to all Redis keys
+        :param string prefix: An optional namespace prefix to be prepended to all DynamoDB keys
         :param dict dynamodb_opts: Optional parameters for configuring the DynamoDB client, as defined in
           the boto3 API; see https://boto3.amazonaws.com/v1/documentation/api/latest/reference/core/session.html#boto3.session.Session.client
         :param CacheConfig caching: Specifies whether local caching should be enabled and if so,
@@ -39,6 +52,10 @@ class Redis(object):
                           max_connections=16,
                           caching=CacheConfig.default()):
         """Creates a Redis-backed implementation of `:class:ldclient.feature_store.FeatureStore`.
+
+        To use this method, you must first install the `redis` package. Then, put the object
+        returned by this method into the `feature_store` property of your client configuration
+        (:class:ldclient.config.Config).
 
         :param string url: The URL of the Redis host; defaults to `DEFAULT_URL`
         :param string prefix: A namespace prefix to be prepended to all Redis keys; defaults to
