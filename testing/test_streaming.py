c6904c763991d90bb5ce99c84d856fedb62fb5bd
Gavin Whelan <gwhelan@launchdarkly.com>
2019-12-31 21:35:29 +0000

    Test that streaming update processor records successful and unsuccessful connection attempts in the diagnostic accumulator when available.

--- testing/test_streaming.py
+++ testing/test_streaming.py
@@ -1,6 +1,7 @@
 from threading import Event
 
 from ldclient.config import Config
+from ldclient.diagnostics import _DiagnosticAccumulator
 from ldclient.feature_store import InMemoryFeatureStore
 from ldclient.streaming import StreamingUpdateProcessor
 from ldclient.version import VERSION
@@ -8,6 +9,7 @@ from testing.http_util import start_server
 
 
 fake_event = 'event:put\ndata: {"data":{"flags":{},"segments":{}}}\n\n'
+response_headers = { 'Content-Type': 'text/event-stream' }
 
 # Note that our simple HTTP stub server implementation does not actually do streaming responses, so
 # in these tests the connection will get closed after the response, causing the streaming processor
@@ -20,7 +22,7 @@ def test_uses_stream_uri():
 
     with start_server() as server:
         config = Config(sdk_key = 'sdk-key', stream_uri = server.uri)
-        server.setup_response('/all', 200, fake_event, { 'Content-Type': 'text/event-stream' })
+        server.setup_response('/all', 200, fake_event, response_headers)
 
         with StreamingUpdateProcessor(config, None, store, ready, None) as sp:
             sp.start()
@@ -35,7 +37,7 @@ def test_sends_headers():
 
     with start_server() as server:
         config = Config(sdk_key = 'sdk-key', stream_uri = server.uri)
-        server.setup_response('/all', 200, fake_event, { 'Content-Type': 'text/event-stream' })
+        server.setup_response('/all', 200, fake_event, response_headers)
 
         with StreamingUpdateProcessor(config, None, store, ready, None) as sp:
             sp.start()
@@ -51,7 +53,7 @@ def test_sends_wrapper_header():
     with start_server() as server:
         config = Config(sdk_key = 'sdk-key', stream_uri = server.uri,
                         wrapper_name = 'Flask', wrapper_version = '0.1.0')
-        server.setup_response('/all', 200, fake_event, { 'Content-Type': 'text/event-stream' })
+        server.setup_response('/all', 200, fake_event, response_headers)
 
         with StreamingUpdateProcessor(config, None, store, ready, None) as sp:
             sp.start()
@@ -65,7 +67,7 @@ def test_sends_wrapper_header_without_version():
     with start_server() as server:
         config = Config(sdk_key = 'sdk-key', stream_uri = server.uri,
                         wrapper_name = 'Flask')
-        server.setup_response('/all', 200, fake_event, { 'Content-Type': 'text/event-stream' })
+        server.setup_response('/all', 200, fake_event, response_headers)
 
         with StreamingUpdateProcessor(config, None, store, ready, None) as sp:
             sp.start()
@@ -97,7 +99,7 @@ def test_can_use_https_proxy_via_config():
 def _verify_http_proxy_is_used(server, config):
     store = InMemoryFeatureStore()
     ready = Event()
-    server.setup_response(config.stream_base_uri + '/all', 200, fake_event, { 'Content-Type': 'text/event-stream' })
+    server.setup_response(config.stream_base_uri + '/all', 200, fake_event, response_headers)
     with StreamingUpdateProcessor(config, None, store, ready, None) as sp:
         sp.start()
         # For an insecure proxy request, our stub server behaves enough like the real thing to satisfy the
@@ -111,10 +113,44 @@ def _verify_http_proxy_is_used(server, config):
 def _verify_https_proxy_is_used(server, config):
     store = InMemoryFeatureStore()
     ready = Event()
-    server.setup_response(config.stream_base_uri + '/all', 200, fake_event, { 'Content-Type': 'text/event-stream' })
+    server.setup_response(config.stream_base_uri + '/all', 200, fake_event, response_headers)
     with StreamingUpdateProcessor(config, None, store, ready, None) as sp:
         sp.start()
         # Our simple stub server implementation can't really do HTTPS proxying, so the request will fail, but
         # it can still record that it *got* the request, which proves that the request went to the proxy.
         req = server.await_request()
         assert req.method == 'CONNECT'
+
+def test_records_diagnostic_on_stream_init_success():
+    store = InMemoryFeatureStore()
+    ready = Event()
+    with start_server() as server:
+        config = Config(sdk_key = 'sdk-key', stream_uri = server.uri)
+        server.setup_response('/all', 200, fake_event, response_headers)
+        diag_accum = _DiagnosticAccumulator(1)
+
+        with StreamingUpdateProcessor(config, None, store, ready, diag_accum) as sp:
+            sp.start()
+            server.await_request()
+            server.await_request()
+            recorded_inits = diag_accum.create_event_and_reset(0, 0)['streamInits']
+
+            assert len(recorded_inits) == 1
+            assert recorded_inits[0]['failed'] is False
+
+def test_records_diagnostic_on_stream_init_failure():
+    store = InMemoryFeatureStore()
+    ready = Event()
+    with start_server() as server:
+        config = Config(sdk_key = 'sdk-key', stream_uri = server.uri)
+        server.setup_response('/all', 200, 'event:put\ndata: {\n\n', response_headers)
+        diag_accum = _DiagnosticAccumulator(1)
+
+        with StreamingUpdateProcessor(config, None, store, ready, diag_accum) as sp:
+            sp.start()
+            server.await_request()
+            server.await_request()
+            recorded_inits = diag_accum.create_event_and_reset(0, 0)['streamInits']
+
+            assert len(recorded_inits) == 1
+            assert recorded_inits[0]['failed'] is True
