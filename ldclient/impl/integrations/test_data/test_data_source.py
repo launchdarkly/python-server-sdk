dd7561e5dd14df970fb65d1aa9a86412a093b4e9
charukiewicz <charukiewicz@protonmail.com>
2021-11-23 22:46:20 +0000

    finished FlagBuilder implementation and added FlagRuleBuilder implementation

--- ldclient/impl/integrations/test_data/test_data_source.py
+++ ldclient/impl/integrations/test_data/test_data_source.py
@@ -1,3 +1,4 @@
+import copy
 
 TRUE_VARIATION_INDEX = 0
 FALSE_VARIATION_INDEX = 1
@@ -12,8 +13,36 @@ class _FlagBuilder():
     def __init__(self, key):
         self._key = key
         self._on = True
-        # TODO set up deep copy
         self._variations = []
+
+    def copy(self):
+        to = _FlagBuilder(self._key)
+
+        to._on = self._on
+        to._variations = copy.copy(self._variations)
+
+        try:
+            to._off_variation = self._off_variation
+        except:
+            pass
+
+        try:
+            to._fallthrough_variation = self._fallthrough_variation
+        except:
+            pass
+
+        try:
+            to._targets = copy.copy(self._targets)
+        except:
+            pass
+
+        try:
+            to._rules = copy.copy(self._rules)
+        except:
+            pass
+
+        return to
+
 
     def on(self, aBool):
         self._on = aBool
@@ -49,7 +78,8 @@ class _FlagBuilder():
             and self._variations[FALSE_VARIATION_INDEX] == False)
 
     def variations(self, *variations):
-        self._variations = variations
+        self._variations = list(variations)
+
         return self
 
 
@@ -58,3 +88,134 @@ class _FlagBuilder():
             return self.boolean_flag().variation_for_all_users(variation_for_boolean(variation))
         else:
             return self.on(True).fallthrough_variation(variation)
+
+    def variation_for_user(self, user_key, variation):
+        if isinstance(variation, bool):
+            return self.boolean_flag().variation_for_user(user_key, variation_for_boolean(variation))
+        else:
+            # `variation` specifies the index of the variation to set
+            targets = {}
+            try:
+                targets = self._targets
+            except:
+                self._targets = {}
+
+            for idx, var in enumerate(self._variations):
+                if (idx == variation):
+                    # If there is no set at the current variation, set it to be empty
+                    target_for_variation = []
+                    if idx in targets:
+                        target_for_variation = targets[idx]
+
+                    # If user is not in the current variation set, add them
+                    if user_key not in target_for_variation:
+                        target_for_variation.append(user_key)
+
+                    self._targets[idx] = target_for_variation
+
+                else:
+                    # Remove user from the other variation set if necessary
+                    if idx in targets:
+                        target_for_variation = targets[idx]
+                        if user_key in target_for_variation:
+                            user_key_idx = target_for_variation.index(user_key)
+                            del target_for_variation[user_key_idx]
+
+                        self._targets[idx] = target_for_variation
+
+            return self
+
+    def add_rule(self, flag_rule_builder):
+        try:
+            len(self._rules) >= 0
+        except:
+            self._rules = []
+
+        self._rules.append(flag_rule_builder)
+
+    def if_match(self, attribute, *values):
+        flag_rule_builder = _FlagRuleBuilder(self)
+        return flag_rule_builder.and_match(attribute, values)
+
+    def if_not_match(self, attribute, *values):
+        flag_rule_builder = _FlagRuleBuilder(self)
+        return flag_rule_builder.and_not_match(attribute, values)
+
+    def build(self, version):
+        base_flag_object = {
+            'key': self._key,
+            'version': version,
+            'on': self._on,
+            'variations': self._variations
+        }
+
+        try:
+            base_flag_object['off_variation'] = self._off_variation
+        except:
+            pass
+
+        try:
+            base_flag_object['fallthrough_variation'] = self._fallthrough_variation
+        except:
+            pass
+
+        try:
+            targets = []
+            for var_index, user_keys in self._targets.items():
+                targets.append({
+                    'variation': var_index,
+                    'values': user_keys
+                })
+            base_flag_object['targets'] = targets
+        except:
+            pass
+
+        try:
+            base_flag_object['rules'] = []
+            for idx, rule in enumerate(self._rules):
+                base_flag_object['rules'].append(rule.build(idx))
+        except:
+            pass
+
+        return base_flag_object
+
+
+class _FlagRuleBuilder():
+    def __init__(self, flag_builder):
+        self._flag_builder = flag_builder
+        self._clauses = []
+        self._variation = None
+
+    def and_match(self, attribute, *values):
+        self._clauses.append({
+                'attribute': attribute,
+                'operator': 'in',
+                'values': list(values),
+                'negate': False
+            })
+        return self
+
+    def and_not_match(self, attribute, *values):
+        self._clauses.append({
+                'attribute': attribute,
+                'operator': 'in',
+                'values': list(values),
+                'negate': True
+            })
+        return self
+
+    def then_return(self, variation):
+        if isinstance(variation, bool):
+            self._flag_builder.boolean_flag()
+            return self.then_return(variation_for_boolean(variation))
+        else:
+            self._variation = variation
+            self._flag_builder.add_rule(self)
+            return self._flag_builder
+
+    def build(self, id):
+        return {
+            'id': 'rule' + str(id),
+            'variation': self._variation,
+            'clauses': self._clauses
+        }
--- testing/test_test_data_source.py
+++ testing/test_test_data_source.py
@@ -11,6 +11,7 @@ from ldclient.versioned_data_kind import FEATURES, SEGMENTS
 
 #from ldclient.integrations import TestData
 from ldclient.impl.integrations.test_data.test_data_source import _FlagBuilder
+from ldclient.impl.integrations.test_data.test_data_source import _FlagRuleBuilder
 
 
 
@@ -32,28 +33,85 @@ def test_makes_flag_builder():
     assert flagBuilder._on is True
     assert flagBuilder._variations == []
 
-def test_can_turn_flag_off():
+def test_flagbuilder_can_turn_flag_off():
     flagBuilder = _FlagBuilder('test-flag')
     flagBuilder.on(False)
     assert flagBuilder._on is False
 
-def test_can_set_fallthrough_variation():
+def test_flagbuilder_can_set_fallthrough_variation():
     flagBuilder = _FlagBuilder('test-flag')
     flagBuilder.fallthrough_variation(2)
     assert flagBuilder._fallthrough_variation == 2
 
-def test_can_set_off_variation():
+def test_flagbuilder_can_set_off_variation():
     flagBuilder = _FlagBuilder('test-flag')
     flagBuilder.off_variation(2)
     assert flagBuilder._off_variation == 2
 
-def test_can_make_boolean_flag():
+def test_flagbuilder_can_make_boolean_flag():
     flagBuilder = _FlagBuilder('boolean-flag').boolean_flag()
     assert flagBuilder._is_boolean_flag() == True
     assert flagBuilder._fallthrough_variation == 0
     assert flagBuilder._off_variation == 1
 
-def test_can_set_variation_for_all_users():
+def test_flagbuilder_can_set_variation_for_all_users():
     flagBuilder = _FlagBuilder('test-flag')
     flagBuilder.variation_for_all_users(True)
     assert flagBuilder._fallthrough_variation == 0
+
+def test_flagbuilder_can_set_variations():
+    flagBuilder = _FlagBuilder('test-flag')
+    flagBuilder.variations(2,3,4,5)
+    assert flagBuilder._variations == [2,3,4,5]
+
+def test_flagbuilder_can_copy():
+    flagBuilder = _FlagBuilder('test-flag')
+    flagBuilder.variations(1,2)
+    flagBuilderCopy = flagBuilder.copy()
+    flagBuilder.variations(3,4)
+    assert flagBuilderCopy._variations == [1,2]
+
+def test_flagbuilder_can_set_boolean_variation_for_user():
+    flagBuilder = _FlagBuilder('user-variation-flag')
+    flagBuilder.variation_for_user('christian', False)
+    assert flagBuilder._targets == {1: ['christian']}
+
+def test_flagbuilder_can_set_numerical_variation_for_user():
+    flagBuilder = _FlagBuilder('user-variation-flag')
+    flagBuilder.variations('a','b','c')
+    flagBuilder.variation_for_user('christian', 2)
+    expected_targets = [
+        {
+            'variation': 2,
+            'values': ['christian']
+        }
+    ]
+    assert flagBuilder.build(1)['targets'] == expected_targets
+
+def test_flagbuilder_can_build():
+    flagBuilder = _FlagBuilder('some-flag')
+    flagRuleBuilder = _FlagRuleBuilder(flagBuilder)
+    flagRuleBuilder.and_match('country', 'fr').then_return(True)
+    expected_result = {
+        'fallthrough_variation': 0,
+        'key': 'some-flag',
+        'off_variation': 1,
+        'on': True,
+        'variations': [True, False],
+        'rules': [
+            {
+                'clauses': [
+                    {'attribute': 'country',
+                    'negate': False,
+                    'operator': 'in',
+                    'values': ['fr']
+                    }
+                ],
+                'id': 'rule0',
+                'variation': 0
+            }
+        ],
+        'version': 1,
+    }
+
+    assert flagBuilder.build(1) == expected_result
