577d51e1092a64d0053725503f50ad0183dc2ad7
Jason Bailey <jbailey@launchdarkly.com>
2026-08-04 10:30:09 -0600

    feat: Add async migration support (#470)

--- ldclient/migrations/__init__.py
+++ ldclient/migrations/__init__.py
@@ -1,8 +1,13 @@
+from .async_migrator import *
 from .migrator import *
 from .tracker import *
 from .types import *
 
 __all__ = [
+    'AsyncMigrationConfig',
+    'AsyncMigrator',
+    'AsyncMigratorBuilder',
+    'AsyncMigratorFn',
     'Migrator',
     'MigratorBuilder',
     'MigratorCompareFn',
